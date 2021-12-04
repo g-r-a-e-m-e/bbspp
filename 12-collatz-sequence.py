ffe7a950f7a69e383a7962ec5b18fac187afc86e
graeme <graeme.t.benson@gmail.com>
2021-12-04 08:47:49 -0500

    update files

--- 12-collatz-sequence.py
+++ 12-collatz-sequence.py
@@ -4,29 +4,51 @@ Collatz Sequence (3n+1 Problem), by Graeme Benson
 Original code can be found at
 https://nostarch.com/big-book-small-python-projects
 """
-import sys
 import time
+import random
+import datetime as dt
 
 print("""\nThe Collatz Sequence, or 3n+1 problem, is a sequence of numbers
 starting from n, following three rules:
-1. If n is even, the next number n is n/2
-2. If n is odd, the next number n is 3n+1
-3. If n is 1, the sequence terminates
+    1. If n is even, the next number n is n/2
+    2. If n is odd, the next number n is 3n+1
+    3. If n is 1, the sequence terminates
 
 It is generally thought, though as of yet mathematically unproven, that every
 starting number n eventually ends at 1.
 
-Let's try it. Enter a number greater than 0:\n""")
+Let's try it.\n""")
 
-n = int(input("> "))
+seconds_to_run = int(input("Enter the number of seconds the program should run: "))
 
-print()
-while n != 1:
-    if n % 2 == 0:
-        n = n //2
-    else:
-        n = 3*n+1
+print("\nRunning...")
 
-    print(str(n) + ", ", end = "", flush = True)
-    time.sleep(0.05)
-print()
+end = dt.datetime.now() + dt.timedelta(seconds = seconds_to_run)
+results = {}
+while dt.datetime.now() <= end:
+    n_list = []
+    n = random.randint(1, 10**10)
+    n_list.append(n)
+    while n != 1:
+        if n % 2 == 0:
+            n = n //2
+            n_list.append(n)
+        else:
+            n = 3*n+1
+            n_list.append(n)
+        result = {n_list[0]: len(n_list)}
+        results.update(result)
+
+max_int = max(results, key = lambda x: results[x])
+max_len = results[max_int]
+num_ints = len(results)
+
+print("\nThe integer with the longest sequence is " \
+        +  str(max_int) \
+        + " with a total of " \
+        + str(max_len) \
+        + " steps to 1. " \
+        + str(num_ints) \
+        + " integers were tested in " \
+        + str(seconds_to_run) \
+        + " seconds.\n")
--- README.md
+++ README.md
@@ -4,3 +4,4 @@
 - 01: *Cold-Warm-Hot* - a deductive logic game
 - 02: *Birthday Problem* - mathematical representation of *n* people sharing a birthday
 - 04: *Blackjack* - Blackjack, also known as 21
+- 12: *Collatz Sequence* - A timed simulation of the Collatz Sequence
